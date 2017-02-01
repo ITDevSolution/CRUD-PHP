51e8d09829b3b2421dfce8a60c3dc0e49f6b11d0
joel ronceros <jronceros.byj@gmail.com>
2017-01-31 22:43:25 -0500

    Se modifico el mensaje de conexion

--- conexion.php
+++ conexion.php
@@ -1,11 +1,10 @@
 <?php 
 $conexion = new mysqli('localhost','root','toor','prueba');
 
-if ($conexion->connect_error) {
+/*if ($conexion->connect_error) {
 	die('error en la conexion'. $conexion->connect_error);
 }else{
 printf("Se establecio conexion correctamente: %s\n",$conexion->server_info);	
-}
-$conexion->close();
+}*/
 
 ?>
